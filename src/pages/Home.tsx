76c54c1f524ef2242f64d061d65d48c7635ff123
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-28 11:05:13 +0700

    :hammer: refactor code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { GameListContainer } from 'components/GameList/GameList.container'
 import React from 'react'
-import { withErrorBoundary } from 'hoc/withErrorBoundary'
 
 const Home: React.FC = () => {
 	return (
